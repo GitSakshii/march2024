e9e86635ff8ddff26fd5347df267d4fdefe23922
GitSakshii <157839341+GitSakshii@users.noreply.github.com>
2024-03-12 11:14:45 +0530

    Create Playback.sql

---
+++ 11march/Playback.sql
@@ -0,0 +1,26 @@
+use sqlday3;
+create table playback(
+session_id int  PRIMARY KEY,
+customer_id int,
+start_time int,
+end_time int,
+CONSTRAINT check_start_and_end_time CHECK(start_time <= end_time));
+INSERT INTO playback
+values(1, 1, 1, 5),
+(2, 1, 15, 23),
+(3, 2, 10, 12),
+(4, 2, 17, 28),
+(5, 2, 2, 8);
+
+create table ads( ad_id int PRIMARY KEY ,
+customer_id  int,
+ timestamp  int );
+ insert into ads
+ values(1,1,5),(2,2,17),(3,2,20);
+ select session_id
+ from playback as p
+ left join ads as a
+ on p.customer_id=a.customer_id
+ and a.timestamp Between p.start_time AND p.end_time
+ where a.customer_id is Null;
+ 

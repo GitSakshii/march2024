e8ff4148e713b6011f6a59272ac1bea21e5a6823
GitSakshii <157839341+GitSakshii@users.noreply.github.com>
2024-03-15 10:24:53 +0530

    Delete 15march directory

--- 15march/Q3.sql
+++
@@ -1,6 +0,0 @@
--- Tables have already been created in the previous questions
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS name, e.job_id, e.department_id, d.department_name
-FROM employees e
-JOIN departments d ON e.department_id = d.department_id
-JOIN locations l ON d.location_id = l.location_id
-WHERE l.city = 'London';

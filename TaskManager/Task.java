67c8d80c44f3d2da19f835ed8b54703ac6b632c6
GitSakshii <157839341+GitSakshii@users.noreply.github.com>
2024-03-18 12:27:19 +0530

    Update Task.java

--- TaskManager/Task.java
+++ TaskManager/Task.java
@@ -8,8 +8,20 @@ public class Task {
         this.title=title;
         this.duedate=duedate;
         this.Description=Description;
-        this.Status="Pending";//Be default it is pending
+        this.Status="Pending";//Be default the status is set to pending
 
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+    public String getStatus()
+    {
+        return Status;
+    }
+    public void setStatus(String status)
+    {
+        Status=status;
     }
     @Override
     public String toString()

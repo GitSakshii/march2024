15c052d581d9b193c899df473bf2caff1af79ae0
GitSakshii <157839341+GitSakshii@users.noreply.github.com>
2024-03-18 12:29:17 +0530

    Update Student.java

--- TaskManager/Student.java
+++ TaskManager/Student.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Student {
     String Name;
@@ -31,6 +32,57 @@ public class Student {
         for(Task task :tasks)
         {
             System.out.println(task);
+        }
+    }
+    public void submitTask(Scanner scanner)
+    {
+        if(tasks.size()!=0)
+        {
+            viewTasks();
+            System.out.println("Select the Task which you want to submit");
+            String title = scanner.next();
+            Task taskToSubmit= null;
+            for(Task task: tasks)
+            {
+                if(task.getTitle().equals(title))
+                {
+                    taskToSubmit= task;
+                    break;
+                }
+            }
+            if(taskToSubmit!=null)
+            {
+                taskToSubmit.setStatus("submitted");
+                System.out.println("Task has been Submitted");
+            }
+            else
+            {
+                System.out.println(" Enter a valid task");
+            }
+        }
+        else
+        {
+            System.out.println("No Task has been assigned to this user!!");
+        }
+    }
+    public void viewPendingTasks()
+    {
+        for(Task task:tasks)
+        {
+            if(task.getStatus().equals("Pending"))
+            {
+                System.out.println(task);
+            }
+        }
+    }
+    public void viewSubmittedTasks()
+    {
+        for (Task task:tasks)
+        {
+            if(task.getStatus().equals("Submitted"))
+            {
+                System.out.println(task);
+            }
         }
     }
     @Override
